d572395bfbac8a30a93d06925ff012ae52c08904
Andreas Monitzer <andreas@monitzer.com>
2025-05-08 17:50:15 +0200

    Implemented deserializing lists for types where we can actually do that (everything except lists of lists and structs).

--- src/types/seq.rs
+++ src/types/seq.rs
@@ -2,6 +2,8 @@ use capnp::introspect::TypeVariant;
 use serde::de::{DeserializeSeed, SeqAccess, Visitor};
 use tracing::{error, trace};
 
+use crate::types::enums::EnumVisitor;
+
 use super::{list_element::ElementSeed, type_variant_to_str};
 
 // Sequences only know their length at deserialization time, so we have to delay
@@ -72,10 +74,106 @@ where
                     }
                 }
             }
+            Ok(())
         } else {
-            todo!()
+            match self.inner_ty.which() {
+                TypeVariant::Void => {
+                    let mut count = 0;
+                    while seq.next_element::<()>()?.is_some() {
+                        count += 1;
+                    }
+                    (self.generator)(count)
+                        .inspect_err(|err| error!("{err}"))
+                        .map_err(serde::de::Error::custom)
+                        .map(|_| ())
+                }
+                TypeVariant::Bool => iterate_simple::<'_, '_, bool, _, _>(self.generator, seq),
+                TypeVariant::Int8 => iterate_simple::<'_, '_, i8, _, _>(self.generator, seq),
+                TypeVariant::Int16 => iterate_simple::<'_, '_, i16, _, _>(self.generator, seq),
+                TypeVariant::Int32 => iterate_simple::<'_, '_, i32, _, _>(self.generator, seq),
+                TypeVariant::Int64 => iterate_simple::<'_, '_, i64, _, _>(self.generator, seq),
+                TypeVariant::UInt8 => iterate_simple::<'_, '_, u8, _, _>(self.generator, seq),
+                TypeVariant::UInt16 => iterate_simple::<'_, '_, u16, _, _>(self.generator, seq),
+                TypeVariant::UInt32 => iterate_simple::<'_, '_, u32, _, _>(self.generator, seq),
+                TypeVariant::UInt64 => iterate_simple::<'_, '_, u64, _, _>(self.generator, seq),
+                TypeVariant::Float32 => iterate_simple::<'_, '_, f32, _, _>(self.generator, seq),
+                TypeVariant::Float64 => iterate_simple::<'_, '_, f64, _, _>(self.generator, seq),
+                TypeVariant::Text => {
+                    let mut values = Vec::new();
+                    while let Some(value) = seq.next_element::<String>()? {
+                        values.push(value);
+                    }
+                    let mut list_builder = (self.generator)(values.len() as _)
+                        .inspect_err(|err| error!("{err}"))
+                        .map_err(serde::de::Error::custom)?;
+                    for (index, value) in values.into_iter().enumerate() {
+                        let capnp::dynamic_value::Builder::Text(mut text_builder) = list_builder
+                            .reborrow()
+                            .init(index as u32, value.len() as u32)
+                            .inspect_err(|err| error!("{err}"))
+                            .map_err(serde::de::Error::custom)?
+                        else {
+                            return Err(serde::de::Error::custom("Internal error".to_owned()));
+                        };
+                        text_builder.push_str(&value);
+                    }
+                    Ok(())
+                }
+                TypeVariant::Data => {
+                    let mut values = Vec::new();
+                    while let Some(value) = seq.next_element::<serde_bytes::ByteBuf>()? {
+                        values.push(value);
+                    }
+                    let mut list_builder = (self.generator)(values.len() as _)
+                        .inspect_err(|err| error!("{err}"))
+                        .map_err(serde::de::Error::custom)?;
+                    for (index, value) in values.into_iter().enumerate() {
+                        list_builder
+                            .reborrow()
+                            .set(
+                                index as u32,
+                                capnp::dynamic_value::Reader::Data(value.as_ref()),
+                            )
+                            .inspect_err(|err| error!("{err}"))
+                            .map_err(serde::de::Error::custom)?
+                    }
+                    Ok(())
+                }
+                TypeVariant::Enum(raw_enum_schema) => {
+                    let mut values = Vec::new();
+                    while seq
+                        .next_element_seed(EnumVisitor::new(raw_enum_schema.into(), |value| {
+                            values.push(value);
+                        }))?
+                        .is_some()
+                    {}
+                    let mut list_builder = (self.generator)(values.len() as _)
+                        .inspect_err(|err| error!("{err}"))
+                        .map_err(serde::de::Error::custom)?;
+                    for (index, value) in values.into_iter().enumerate() {
+                        list_builder
+                            .reborrow()
+                            .set(
+                                index as u32,
+                                capnp::dynamic_value::Reader::Enum(
+                                    capnp::dynamic_value::Enum::new(
+                                        value.get_ordinal(),
+                                        value.get_containing_enum(),
+                                    ),
+                                ),
+                            )
+                            .inspect_err(|err| error!("{err}"))
+                            .map_err(serde::de::Error::custom)?
+                    }
+                    Ok(())
+                }
+                TypeVariant::Struct(_) | TypeVariant::List(_) => Err(serde::de::Error::custom(
+                    "Cap'n Proto encoding requires pointer lists to declare their size before the actual data. Your decoder does not provide this information.",
+                )),
+                TypeVariant::AnyPointer => unimplemented!(),
+                TypeVariant::Capability => unimplemented!(),
+            }
         }
-        Ok(())
     }
 }
 
@@ -94,4 +192,27 @@ where
             .inspect_err(|err| error!("{err}"))?;
         Ok(())
     }
+}
+
+fn iterate_simple<'a, 'de, Value, F, A>(generator: F, mut seq: A) -> Result<(), A::Error>
+where
+    Value: serde::Deserialize<'de>,
+    A: SeqAccess<'de>,
+    for<'b> capnp::dynamic_value::Reader<'b>: From<Value>,
+    F: FnOnce(u32) -> capnp::Result<capnp::dynamic_list::Builder<'a>>,
+{
+    let mut values = Vec::new();
+    while let Some(value) = seq.next_element::<Value>()? {
+        values.push(value);
+    }
+    let mut list_builder = generator(values.len() as _)
+        .inspect_err(|err| error!("{err}"))
+        .map_err(serde::de::Error::custom)?;
+    for (index, value) in values.into_iter().enumerate() {
+        list_builder
+            .set(index as u32, value.into())
+            .inspect_err(|err| error!("{err}"))
+            .map_err(serde::de::Error::custom)?;
+    }
+    Ok(())
 }
